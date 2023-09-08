1c1421e22fc3783e87239a0502d863bea77a531e
Sebastian Cyrych <scyrych@uw.co.uk>
2023-09-08 10:40:32 +0200

    customize statusbar and remove buttons from tabs

--- chadrc.lua
+++ chadrc.lua
@@ -4,6 +4,16 @@
    theme = 'catppuccin',
    statusline = {
      theme = 'vscode_colored',
+     overriden_modules = function(modules)
+      table.remove(modules, 9) --remove cursor_positioning
+      table.remove(modules, 9) --remove file_encoding
+      table.remove(modules, 9) --remove filetype
+     end,
+   },
+   tabufline = {
+     overriden_modules = function(modules)
+       table.remove(modules, 4) --remove buttons
+     end,
    },
  }
  M.plugins = 'custom.plugins'
